77875677e84ad76824d9850fb9a10319e444f48c
gwatts <gwatts@47dd0619-b7a8-462c-89d8-56395fd97e59>
2011-09-14 09:18:27 +0000

    Windows stuff to run the tests and do development.
    git-svn-id: https://svn.cern.ch/reps/atlasperf/CombPerf/FlavorTag/JetTagPerformanceCalibration/Combination/trunk@207120 47dd0619-b7a8-462c-89d8-56395fd97e59

---
+++ WinBuild/ParserTest/BasicTests.cpp
@@ -0,0 +1,50 @@
+#include "stdafx.h"
+
+#pragma managed (push, off)
+#include <Combination/Parser.h>
+#pragma managed (pop)
+
+#include <vector>
+using namespace std;
+using namespace BTagCombination;
+
+using namespace System;
+using namespace System::Text;
+using namespace System::Collections::Generic;
+using namespace Microsoft::VisualStudio::TestTools::UnitTesting;
+
+namespace ParserTest
+{
+	[TestClass]
+	public ref class BasicTests
+	{
+	private:
+		TestContext^ testContextInstance;
+
+	public: 
+		[TestMethod]
+		void TestEmptyString()
+		{
+			vector<CalibrationAnalysis> result (Parse(""));
+			Assert::AreEqual( (unsigned int) 0, result.size(), "# of found analyses");
+		};
+
+		[TestMethod]
+		void TestAnalysisWithNoBin()
+		{
+			vector<CalibrationAnalysis> result (Parse("Analysis(ptrel, bottom, SV050){}"));
+			Assert::AreEqual((unsigned int) 1, result.size(), "# of found analyses");
+			Assert::AreEqual((unsigned int) 0, result[0].bins.size(), "# of bins");
+		}
+
+		[TestMethod]
+		void TestAnalysisWithOneBin()
+		{
+			vector<CalibrationAnalysis> result (Parse("Analysis(ptrel, bottom, SV050){bin(5<pt<30)}"));
+			Assert::AreEqual((unsigned int) 1, result.size(), "# of found analyses");
+			Assert::AreEqual((unsigned int) 1, result[0].bins.size(), "# of bins");
+			auto bb = result[0].bins[0];
+			Assert::AreEqual((unsigned int) 1, bb.binSpec.size(), "# of bins");
+		}
+	};
+}
