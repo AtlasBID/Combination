150d459236cd53ca192c0247a3947b75d6de7586
gwatts <gwatts@47dd0619-b7a8-462c-89d8-56395fd97e59>
2011-09-15 13:21:53 +0000

    More tests to look at bin boundaries
    git-svn-id: https://svn.cern.ch/reps/atlasperf/CombPerf/FlavorTag/JetTagPerformanceCalibration/Combination/trunk@207410 47dd0619-b7a8-462c-89d8-56395fd97e59

--- WinBuild/ParserTest/BasicTests.cpp
+++ WinBuild/ParserTest/BasicTests.cpp
@@ -45,6 +45,21 @@ namespace ParserTest
 			Assert::AreEqual((unsigned int) 1, result[0].bins.size(), "# of bins");
 			auto bb = result[0].bins[0];
 			Assert::AreEqual((unsigned int) 1, bb.binSpec.size(), "# of bins");
+			Assert::IsTrue(bb.binSpec[0].variable == "pt", "incorrect bin name");
+			Assert::AreEqual(5.0, bb.binSpec[0].lowvalue, "low value");
+			Assert::AreEqual(30.0, bb.binSpec[0].highvalue, "high value");
+		}
+
+		[TestMethod]
+		void TestAnalysisWithTwoBins()
+		{
+			vector<CalibrationAnalysis> result (Parse("Analysis(ptrel, bottom, SV050){bin(5<pt<30, 20<eta<44)}"));
+			Assert::AreEqual((unsigned int) 1, result.size(), "# of found analyses");
+			Assert::AreEqual((unsigned int) 1, result[0].bins.size(), "# of bins");
+			auto bb = result[0].bins[0];
+			Assert::AreEqual((unsigned int) 2, bb.binSpec.size(), "# of bins");
+			Assert::IsTrue(bb.binSpec[0].variable == "pt", "incorrect bin name");
+			Assert::IsTrue(bb.binSpec[1].variable == "eta", "incorrect bin name");
 		}
 	};
 }
