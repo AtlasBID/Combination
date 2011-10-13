ce03ebbdee5108f3fe0c83d8cf84e1ba5bf140cb
gwatts <gwatts@47dd0619-b7a8-462c-89d8-56395fd97e59>
2011-10-13 06:30:13 +0000

    Prints out summary of an input file
    git-svn-id: https://svn.cern.ch/reps/atlasperf/CombPerf/FlavorTag/JetTagPerformanceCalibration/Combination/trunk@214071 47dd0619-b7a8-462c-89d8-56395fd97e59

--- bin/FTGenerateSummary.cxx
+++ bin/FTGenerateSummary.cxx
@@ -7,12 +7,60 @@
 #include "Combination/Parser.h"
 
 #include <iostream>
-using namespace std;
+#include <sstream>
+#include <fstream>
+#include <string>
+
 #include <boost/spirit/version.hpp>
+
+using namespace std;
+using namespace BTagCombination;
+
+string LoadFromFile (const string &fname);
 
 int main()
 {
-  BTagCombination::Parse("");
-  cout << "hi " << SPIRIT_VERSION  <<endl;
+  string filename = "input.txt";
+  
+  vector<CalibrationAnalysis> items = BTagCombination::Parse(LoadFromFile(filename));
+
+  cout << "See " << items.size() << " analyses" << endl;
+  for (unsigned int iana = 0; iana < items.size(); iana++) {
+    const CalibrationAnalysis &ana (items[iana]);
+    cout << "Analysis " << ana.name
+	 << ", " << ana.flavor
+	 << ", " << ana.operatingPoint
+	 << endl;
+
+    for (unsigned int ibin = 0; ibin < ana.bins.size(); ibin++){
+      const CalibrationBin &bin(ana.bins[ibin]);
+      cout << "  Bin with CV " << bin.centralValue << " +- " << bin.centralValueStatisticalError << endl;
+
+      for(unsigned int isys = 0; isys < bin.systematicErrors.size(); isys++) {
+	const SystematicError &sys(bin.systematicErrors[isys]);
+	cout << "    Sys " << sys.name << " " << sys.value << endl;
+      }
+    }
+  }
+
+
   return 0;
+}
+
+//
+// Load a file into memory
+//
+string LoadFromFile (const string &fname)
+{
+  ifstream input(fname.c_str());
+  ostringstream result;
+
+  while (!input.eof())
+    {
+      string line;
+      getline(input, line);
+      result << line << endl;
+    }
+
+  return result.str();
 }
