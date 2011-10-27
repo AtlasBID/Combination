6eba985198fde9f534ba1f2289ffea84564db94e
gwatts <gwatts@47dd0619-b7a8-462c-89d8-56395fd97e59>
2011-10-27 08:53:51 +0000

    Added code to look for the bins
    git-svn-id: https://svn.cern.ch/reps/atlasperf/CombPerf/FlavorTag/JetTagPerformanceCalibration/Combination/trunk@218504 47dd0619-b7a8-462c-89d8-56395fd97e59

--- src/CDIConverter.cxx
+++ src/CDIConverter.cxx
@@ -6,8 +6,61 @@
 
 #include "CalibrationDataInterface/CalibrationDataContainer.h"
 
+#include <map>
+#include <vector>
+#include <string>
+#include <stdexcept>
+#include <iostream>
+
 using Analysis::CalibrationDataHistogramContainer;
 using Analysis::CalibrationDataContainer;
+
+namespace {
+  using namespace BTagCombination;
+  using namespace std;
+
+  // helper functions and objects for below
+
+  class bin_boundaries2D {
+  };
+
+  typedef map<string, vector<pair<double,double> > > t_bin_list;
+
+  // Go through all the analysis and extract all the bins that are
+  // being used.
+  t_bin_list extract_bins (const CalibrationAnalysis &ana)
+  {
+    t_bin_list result;
+
+    for (unsigned int ibin = 0; ibin < ana.bins.size(); ibin++) {
+      const CalibrationBin &bin (ana.bins[ibin]);
+      for (unsigned int iboundary = 0; iboundary < bin.binSpec.size(); iboundary++) {
+	const CalibrationBinBoundary &bound (bin.binSpec[iboundary]);
+	pair<double,double> bp = make_pair(bound.lowvalue, bound.highvalue);
+	result[bound.variable].push_back(bp);
+      }
+    }
+
+    return result;
+  }
+
+  bin_boundaries2D calcBoundaries (const CalibrationAnalysis &ana)
+  {
+    // Find all the bins that are in the analysis
+    t_bin_list raw_bins = extract_bins(ana);
+    if (raw_bins.size() > 2) {
+      throw runtime_error(("Analysis '" + ana.name + "' has more than 2 bins!").c_str());
+    }
+    if (raw_bins.size() == 0) {
+      throw runtime_error(("Analysis '" + ana.name + "' has no bins!").c_str());
+    }
+
+    cout << "Number of bins is " << raw_bins.size() << endl;
+
+    bin_boundaries2D stuff;
+    return stuff;
+  }
+}
 
 namespace BTagCombination {
 
@@ -23,9 +76,18 @@ namespace BTagCombination {
     // First, convert this analysis to a histogram.
     //
 
+
+    calcBoundaries(eff);
+    // get bin boundaries
+    // get the values 
+    // create a histogram
+
     //
     // now, get the systeamtic histogram for each histogram.
     //
+
+    // get the list of systematics
+    // loop, use bin boundaries from above, etc.
 
     return result;
 
