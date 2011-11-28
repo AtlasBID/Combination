07ad421d38b717fc436f5c19d72d78446de80126
gwatts <gwatts@47dd0619-b7a8-462c-89d8-56395fd97e59>
2011-11-28 09:58:06 +0000

    More tests to look for bad bin boundaries
    git-svn-id: https://svn.cern.ch/reps/atlasperf/CombPerf/FlavorTag/JetTagPerformanceCalibration/Combination/trunk@229002 47dd0619-b7a8-462c-89d8-56395fd97e59

--- src/Combiner.cxx
+++ src/Combiner.cxx
@@ -6,9 +6,20 @@
 
 #include <stdexcept>
 
+using namespace std;
+
+namespace {
+  using namespace BTagCombination;
+
+  // Check to see if the two bins are the same or not.
+  bool sameBin (const vector<CalibrationBinBoundary> &b1, const vector<CalibrationBinBoundary> &b2)
+  {
+    return false;
+  }
+}
+
 namespace BTagCombination
 {
-  using namespace std;
 
   CalibrationBin CombineBin (vector<CalibrationBin> &bins)
   {
@@ -22,7 +33,19 @@ namespace BTagCombination
     if (bins.size() == 1)
       return bins[0];
 
-    throw runtime_error ("Not implemented yet");
+    // If there are more than 1 bins, then they all need to be identical!
+    vector<CalibrationBinBoundary> specs (bins[0].binSpec);
+    for (unsigned int ibin = 1; ibin < bins.size(); ibin++)
+      if (!sameBin(specs, bins[ibin].binSpec))
+	throw runtime_error ("Attempt to combine two bins with different boundaries");
+
+    // Now we are ready to build the combination!
+
+    // Now that we have the result, we need to extract the numbers and build the resulting bin
+    CalibrationBin result;
+    result.binSpec = specs;
+
+    return CalibrationBin();
   }
 }
 
--- test/CombinerTest_CppUnit.cxx
+++ test/CombinerTest_CppUnit.cxx
@@ -19,8 +19,14 @@ class CombinerTest : public CppUnit::TestFixture
 {
   CPPUNIT_TEST_SUITE( CombinerTest );
 
-  CPPUNIT_TEST ( testOBOneBinIn );
   CPPUNIT_TEST_EXCEPTION ( testOBZeroBinIn, std::runtime_error );
+  CPPUNIT_TEST_EXCEPTION ( testOBTwoDiffBins, std::runtime_error );
+  CPPUNIT_TEST_EXCEPTION ( testOBTwoDiffComplexBins, std::runtime_error );
+
+  CPPUNIT_TEST ( testOBOneBinIn );
+  //CPPUNIT_TEST ( testOBTwoBinIn );
+  //CPPUNIT_TEST ( testOBTwoComplexBinIn );
+  //CPPUNIT_TEST ( testOBTwoBinInWithSys );
 
   CPPUNIT_TEST_SUITE_END();
 
@@ -51,6 +57,65 @@ class CombinerTest : public CppUnit::TestFixture
     CPPUNIT_ASSERT(result.centralValueStatisticalError == 0.1);
     CPPUNIT_ASSERT(result.binSpec.size() == 1);
     CPPUNIT_ASSERT(result.binSpec[0].variable == "eta");
+  }
+
+  void testOBTwoDiffBins()
+  {
+    // Try to combine two different bins, but the bins are different sized.
+    // We send in one bin to be combined, and it comes back out!
+    CalibrationBin b1;
+    b1.centralValue = 0.5;
+    b1.centralValueStatisticalError = 0.1;
+    CalibrationBinBoundary bound;
+    bound.variable = "eta";
+    bound.lowvalue = 0.0;
+    bound.highvalue = 2.5;
+    b1.binSpec.push_back(bound);
+    
+    CalibrationBin b2;
+    b2.centralValue = 0.5;
+    b2.centralValueStatisticalError = 0.1;
+    bound.lowvalue = 1.0;
+    b2.binSpec.push_back(bound);
+
+    vector<CalibrationBin> bins;
+    bins.push_back(b1);
+    bins.push_back(b2);
+
+    CalibrationBin result = CombineBin(bins);
+  }
+
+  void testOBTwoDiffComplexBins()
+  {
+    // Try to combine two different bins, but the bins are different sized.
+    // We send in one bin to be combined, and it comes back out!
+    CalibrationBin b1;
+    b1.centralValue = 0.5;
+    b1.centralValueStatisticalError = 0.1;
+    CalibrationBinBoundary bound1;
+    bound1.variable = "eta";
+    bound1.lowvalue = 0.0;
+    bound1.highvalue = 2.5;
+    CalibrationBinBoundary bound2;
+    bound2.variable = "pt";
+    bound2.lowvalue = 0.0;
+    bound2.highvalue = 100.0;
+
+    b1.binSpec.push_back(bound1);
+    b1.binSpec.push_back(bound2);
+    
+    CalibrationBin b2;
+    b2.centralValue = 0.5;
+    b2.centralValueStatisticalError = 0.1;
+    bound1.lowvalue = 1.0;
+    b2.binSpec.push_back(bound2);
+    b2.binSpec.push_back(bound1);
+
+    vector<CalibrationBin> bins;
+    bins.push_back(b1);
+    bins.push_back(b2);
+
+    CalibrationBin result = CombineBin(bins);
   }
 
 };
