35a7cb76d49ec4f8e00b766dedcc1b8aea7f03c8
gwatts <gwatts@47dd0619-b7a8-462c-89d8-56395fd97e59>
2012-01-06 22:36:42 +0000

    Tests for looking at bin boundaries and correctly barfing
    git-svn-id: https://svn.cern.ch/reps/atlasperf/CombPerf/FlavorTag/JetTagPerformanceCalibration/Combination/trunk@240349 47dd0619-b7a8-462c-89d8-56395fd97e59

---
+++ test/BinBoundaryUtilsTest_CppUnit.cxx
@@ -0,0 +1,93 @@
+///
+/// CppUnit tests for the Bin boundary utils.
+///
+///
+
+#include "Combination/BinBoundaryUtils.h"
+
+#include <cppunit/extensions/HelperMacros.h>
+#include <cppunit/Exception.h>
+#include <iostream>
+#include <stdexcept>
+#include <sstream>
+
+using namespace std;
+using namespace BTagCombination;
+
+//
+// Test harness/fixture for the parser
+//
+
+class BinBoundaryUtilsTest : public CppUnit::TestFixture
+{
+  CPPUNIT_TEST_SUITE( BinBoundaryUtilsTest );
+
+  CPPUNIT_TEST( testSimpleOneBins );
+  CPPUNIT_TEST_EXCEPTION ( TestOverlapBins, std::runtime_error );
+  CPPUNIT_TEST_EXCEPTION ( TestGappedBins, std::runtime_error );
+
+  CPPUNIT_TEST_SUITE_END();
+
+  void testSimpleOneBins()
+  {
+    CalibrationAnalysis ana;
+    CalibrationBin b1;
+    CalibrationBinBoundary bb1;
+    bb1.lowvalue = 0.0;
+    bb1.highvalue = 1.0;
+    bb1.variable = "pt";
+    b1.binSpec.push_back(bb1);
+    ana.bins.push_back(b1);
+
+    bin_boundaries result (calcBoundaries(ana));
+  }
+
+  void TestOverlapBins()
+  {
+    CalibrationAnalysis ana;
+    CalibrationBin b1;
+
+    CalibrationBinBoundary bb1;
+    bb1.lowvalue = 0.0;
+    bb1.highvalue = 1.0;
+    bb1.variable = "pt";
+    b1.binSpec.push_back(bb1);
+
+    CalibrationBinBoundary bb2;
+    bb2.lowvalue = 0.5;
+    bb2.highvalue = 1.5;
+    bb2.variable = "pt";
+    b1.binSpec.push_back(bb2);
+
+    ana.bins.push_back(b1);
+
+    bin_boundaries result (calcBoundaries(ana));
+  }
+
+  void TestGappedBins ()
+  {
+    CalibrationAnalysis ana;
+    CalibrationBin b1;
+
+    CalibrationBinBoundary bb1;
+    bb1.lowvalue = 0.0;
+    bb1.highvalue = 1.0;
+    bb1.variable = "pt";
+    b1.binSpec.push_back(bb1);
+
+    CalibrationBinBoundary bb2;
+    bb2.lowvalue = 2.0;
+    bb2.highvalue = 3.0;
+    bb2.variable = "pt";
+    b1.binSpec.push_back(bb2);
+
+    ana.bins.push_back(b1);
+
+    bin_boundaries result (calcBoundaries(ana));
+  }
+};
+
+CPPUNIT_TEST_SUITE_REGISTRATION(BinBoundaryUtilsTest);
+
+// The common atlas test driver
+//#include <TestPolicy/CppUnit_testdriver.cxx>
