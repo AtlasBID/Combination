e494753a3f614fe1636a28d67072c04a9208b6e0
gwatts <gwatts@47dd0619-b7a8-462c-89d8-56395fd97e59>
2012-09-26 23:54:59 +0000

    Tests for some partial correlations
    git-svn-id: https://svn.cern.ch/reps/atlasperf/CombPerf/FlavorTag/JetTagPerformanceCalibration/Combination/trunk@290349 47dd0619-b7a8-462c-89d8-56395fd97e59

--- test/MeasurementUtilsTest_CppUnit.cxx
+++ test/MeasurementUtilsTest_CppUnit.cxx
@@ -32,6 +32,9 @@ class MeasurementUtilsTest : public CppUnit::TestFixture
   CPPUNIT_TEST( testCovarM1TwoC );
   CPPUNIT_TEST( testCovarM2TwoC );
 
+  CPPUNIT_TEST( testCovarM1TwoCPartial );
+  CPPUNIT_TEST( testCovarM2TwoCPartial );
+
   CPPUNIT_TEST_SUITE_END();
 
   void testCovarM1One()
@@ -149,6 +152,57 @@ class MeasurementUtilsTest : public CppUnit::TestFixture
     CPPUNIT_ASSERT_DOUBLES_EQUAL(0.5*0.25, r(1,0), 0.01);
     CPPUNIT_ASSERT_DOUBLES_EQUAL(0.5*0.25, r(0,1), 0.01);
   }
+
+  void testCovarM1TwoCPartial()
+  {
+    // Two guys, with 50% correlation.
+
+    CombinationContext c;
+    Measurement *m1 = c.AddMeasurement ("average", -10.0, 10.0, 5.0, 0.0);
+    m1->addSystematicAbs("e1", 0.5);
+    m1->addSystematicAbs("e2", 0.5);
+    Measurement *m2 = c.AddMeasurement ("average", -10.0, 10.0, 5.0, 0.0);
+    m2->addSystematicAbs("e1", 0.25);
+    m2->addSystematicAbs("e3", 0.25);
+
+    vector<Measurement*> mlist;
+    mlist.push_back(m1);
+    mlist.push_back(m2);
+
+    TMatrixTSym<double> r (CalcCovarMatrixUsingRho (mlist));
+
+    CPPUNIT_ASSERT_EQUAL (2, r.GetNrows());
+    CPPUNIT_ASSERT_DOUBLES_EQUAL(0.5*0.5*2.0, r(0,0), 0.01);
+    CPPUNIT_ASSERT_DOUBLES_EQUAL(0.25*0.25*2.0, r(1,1), 0.01);
+    CPPUNIT_ASSERT_DOUBLES_EQUAL(0.5*0.25, r(1,0), 0.01);
+    CPPUNIT_ASSERT_DOUBLES_EQUAL(0.5*0.25, r(0,1), 0.01);
+  }
+
+  void testCovarM2TwoCPartial()
+  {
+    // Two guys, with 50% correlation.
+
+    CombinationContext c;
+    Measurement *m1 = c.AddMeasurement ("average", -10.0, 10.0, 5.0, 0.0);
+    m1->addSystematicAbs("e1", 0.5);
+    m1->addSystematicAbs("e2", 0.5);
+    Measurement *m2 = c.AddMeasurement ("average", -10.0, 10.0, 5.0, 0.0);
+    m2->addSystematicAbs("e1", 0.25);
+    m2->addSystematicAbs("e3", 0.25);
+
+    vector<Measurement*> mlist;
+    mlist.push_back(m1);
+    mlist.push_back(m2);
+
+    TMatrixTSym<double> r (CalcCovarMatrixUsingComposition (mlist));
+
+    CPPUNIT_ASSERT_EQUAL (2, r.GetNrows());
+    CPPUNIT_ASSERT_DOUBLES_EQUAL(0.5*0.5*2.0, r(0,0), 0.01);
+    CPPUNIT_ASSERT_DOUBLES_EQUAL(0.25*0.25*2.0, r(1,1), 0.01);
+    CPPUNIT_ASSERT_DOUBLES_EQUAL(0.5*0.25, r(1,0), 0.01);
+    CPPUNIT_ASSERT_DOUBLES_EQUAL(0.5*0.25, r(0,1), 0.01);
+  }
+
 };
 
 CPPUNIT_TEST_SUITE_REGISTRATION(MeasurementUtilsTest);
