814e8a24add3df1a703ae88cf8aa7602a7b6c923
gwatts <gwatts@47dd0619-b7a8-462c-89d8-56395fd97e59>
2012-10-01 07:58:16 +0000

    Store plotting results from each guy
    git-svn-id: https://svn.cern.ch/reps/atlasperf/CombPerf/FlavorTag/JetTagPerformanceCalibration/Combination/trunk@290379 47dd0619-b7a8-462c-89d8-56395fd97e59

--- bin/FTExploreFit.cxx
+++ bin/FTExploreFit.cxx
@@ -9,13 +9,43 @@
 #include "Combination/CommonCommandLineUtils.h"
 #include "Combination/Combiner.h"
 #include "Combination/BinUtils.h"
+#include "Combination/Plots.h"
 
 #include <RooMsgService.h>
+#include <TFile.h>
+
+#include <algorithm>
 
 using namespace std;
 using namespace BTagCombination;
 
 void usage(void);
+
+namespace {
+
+  // 
+  // Dump plots into an output file.
+  //
+  void DumpPlotResults (TDirectory *outDir,
+			const CalibrationInfo &info,
+			const vector<CalibrationAnalysis> &r)
+  {
+    vector<CalibrationAnalysis> anas (info.Analyses);
+    anas.insert(anas.end(), r.begin(), r.end());
+    DumpPlots (outDir, anas);
+  }
+
+  string Normalize (const string &name)
+  {
+    string r (name);
+
+    replace(r.begin(), r.end(), '.', '-');
+    replace(r.begin(), r.end(), ':', '-');
+
+    return r;
+  }
+
+}
 
 int main (int argc, char **argv)
 {
@@ -37,6 +67,12 @@ int main (int argc, char **argv)
   RooMsgService::instance().setSilentMode(true);
   RooMsgService::instance().setGlobalKillBelow(RooFit::ERROR);
 
+  //
+  // We will store the outputs in file.
+  //
+
+  TFile *outputPlots = TFile::Open("explore.root", "RECREATE");
+
   // What we do can only be done by looking at one fit at a time. So we
   // have to split everything by flavor, tagger, op, and jet algorithm.
 
@@ -52,6 +88,9 @@ int main (int argc, char **argv)
       continue;
     }
 
+    // Put each set of analyses in a seperate guy.
+    TDirectory *outDir = outputPlots->mkdir(Normalize(i_ana->first).c_str());
+
     // Next, build up a new Calibration Info as the central guy - this will have just one fit. And then
     // get the complete results of the fit.
 
@@ -64,6 +103,10 @@ int main (int argc, char **argv)
 
     double centralChi2 = centralResult[0].metadata["gchi2"]/centralResult[0].metadata["gndof"];
     cout << "  chi2/ndof = " << centralChi2 << endl;
+
+    // Sore the default in a single one.
+
+    DumpPlotResults (outDir->mkdir("Default"), centralInfo, centralResult);
 
     // Now, if we've been asked to remove a bin at a time.
 
@@ -78,11 +121,21 @@ int main (int argc, char **argv)
       cout << "Doing fit without bin " << OPBinName(tempBinInfo) << endl;
       vector<CalibrationAnalysis> missingBinResult (CombineAnalyses(missingBinInfo, false));
 
+      // Print out the chi2
       double missingChi2 = missingBinResult[0].metadata["gchi2"]/missingBinResult[0].metadata["gndof"];
       cout << "  Missing bin chi2/ndof = " << missingChi2 << endl;
 
+      // Generate plots.
+      DumpPlotResults (outDir->mkdir(Normalize(OPBinName(tempBinInfo)).c_str()), missingBinInfo, missingBinResult);
     }
   }
+
+  //
+  // Make sure all plot get written out.
+  //
+
+  outputPlots->Write();
+  outputPlots->Close();
 
   return 0;
 }
