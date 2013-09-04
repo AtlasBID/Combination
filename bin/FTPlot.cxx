666f078cb6bb39058429115701fabcd77473ac58
gwatts <gwatts@47dd0619-b7a8-462c-89d8-56395fd97e59>
2013-09-04 22:54:53 +0000

    Better error messages
    git-svn-id: https://svn.cern.ch/reps/atlasperf/CombPerf/FlavorTag/JetTagPerformanceCalibration/Combination/trunk@295342 47dd0619-b7a8-462c-89d8-56395fd97e59

--- bin/FTPlot.cxx
+++ bin/FTPlot.cxx
@@ -42,6 +42,7 @@ int main (int argc, char **argv)
       } else if (arg == "EffOnly") {
 	whatToPlot = pcEffOnly;
       } else {
+	cout << "Unknown option '" << arg << "'." << endl;
 	usage();
 	return 1;
       }
