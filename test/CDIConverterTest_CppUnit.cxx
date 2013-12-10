6b682b8bb981df687611b98c4d1ff4c77a406aee
gwatts <gwatts@47dd0619-b7a8-462c-89d8-56395fd97e59>
2013-12-10 00:19:02 +0000

    First tests to look for extended information being insterted into the CDI
    git-svn-id: https://svn.cern.ch/reps/atlasperf/CombPerf/FlavorTag/JetTagPerformanceCalibration/Combination/trunk@296539 47dd0619-b7a8-462c-89d8-56395fd97e59

--- test/CDIConverterTest_CppUnit.cxx
+++ test/CDIConverterTest_CppUnit.cxx
@@ -31,6 +31,11 @@ class CDIConverterTest : public CppUnit::TestFixture
   CPPUNIT_TEST( testUncorrelatedErrors );
   CPPUNIT_TEST( testForSystematics );
 
+  CPPUNIT_TEST( testExtendedBinNormalArea );
+  CPPUNIT_TEST( testExtendedBinExtendedArea );
+
+  CPPUNIT_TEST( testBeyondTheEdge );
+
   CPPUNIT_TEST_SUITE_END();
 
   void testBasicGetSF()
@@ -272,6 +277,138 @@ class CDIConverterTest : public CppUnit::TestFixture
     CPPUNIT_ASSERT(c != 0);
     CPPUNIT_ASSERT_EQUAL(true, c->isBinCorrelated("err"));
     CPPUNIT_ASSERT_EQUAL(false, c->isBinCorrelated("uerr"));
+  }
+
+  CalibrationAnalysis generateExtendedBinnedAna()
+  {
+    CalibrationAnalysis ana;
+    ana.name = "ana1";
+    ana.flavor = "bottom";
+    ana.tagger = "SV0";
+    ana.operatingPoint = "0.1";
+    ana.jetAlgorithm = "AntiKt";
+
+    // The normal bin
+
+    CalibrationBin b1;
+    b1.centralValue = 1.1;
+    b1.centralValueStatisticalError = 0.2;
+    CalibrationBinBoundary bb1;
+    bb1.lowvalue = 0.0;
+    bb1.highvalue = 100.0;
+    bb1.variable = "pt";
+    b1.binSpec.push_back(bb1);
+    bb1.lowvalue = 0.0;
+    bb1.highvalue = 2.5;
+    bb1.variable = "abseta";
+    b1.binSpec.push_back(bb1);
+
+    SystematicError e;
+    e.name = "cerr";
+    e.uncorrelated = false;
+    b1.systematicErrors.push_back(e);
+    
+    ana.bins.push_back(b1);
+
+    // The bin with the extended error
+
+    CalibrationBin b2;
+    b2.centralValue = 1.2;
+    b2.centralValueStatisticalError = 0.3;
+    b2.isExtended = true;
+
+    bb1.lowvalue = 100.0;
+    bb1.highvalue = 200.0;
+    bb1.variable = "pt";
+    b2.binSpec.push_back(bb1);
+    bb1.lowvalue = 0.0;
+    bb1.highvalue = 2.5;
+    bb1.variable = "abseta";
+    b2.binSpec.push_back(bb1);
+
+    e.name = "extendederror";
+    e.uncorrelated = true;
+    e.value = 0.2;
+
+    ana.bins.push_back(b2);
+    return ana;
+  }
+
+  void testExtendedBinExtendedArea()
+  {
+    CalibrationAnalysis ana (generateExtendedBinnedAna());
+
+    CalibrationDataContainer *craw = ConvertToCDI (ana, "bogus");
+    CalibrationDataHistogramContainer *c = dynamic_cast<CalibrationDataHistogramContainer *>(craw);
+
+    Analysis::CalibrationDataVariables v;
+    v.jetPt = 150.e3;
+    v.jetEta = -1.1;
+    v.jetAuthor = "AntiKt4Topo";
+
+    TObject *obj = nullptr;
+    double r;
+    CalibrationDataContainer::CalibrationStatus stat = c->getResult(v, r, obj);
+
+    CPPUNIT_ASSERT_EQUAL (CalibrationDataContainer::kSuccess, stat);
+    CPPUNIT_ASSERT_DOUBLES_EQUAL (1.2, r, 0.001);
+
+    stat = c->getStatUncertainty(v, r);
+    CPPUNIT_ASSERT_EQUAL (CalibrationDataContainer::kSuccess, stat);
+    CPPUNIT_ASSERT_DOUBLES_EQUAL (0.3, r, 0.001);
+
+    map<string, UncertaintyResult> allerrors;
+    stat = c->getUncertainties(v, allerrors);
+    //CPPUNIT_ASSERT_EQUAL((size_t)5, allerrors.size());
+
+    CPPUNIT_ASSERT(allerrors.find("cerr") != allerrors.end());
+    CPPUNIT_ASSERT(allerrors.find("result") != allerrors.end());
+    CPPUNIT_ASSERT(allerrors.find("statistics") != allerrors.end());
+    CPPUNIT_ASSERT(allerrors.find("systematics") != allerrors.end());
+    CPPUNIT_ASSERT(allerrors.find("extendederror") != allerrors.end());
+  }
+
+  void testExtendedBinNormalArea()
+  {
+    CalibrationAnalysis ana (generateExtendedBinnedAna());
+
+    CalibrationDataContainer *craw = ConvertToCDI (ana, "bogus");
+    CalibrationDataHistogramContainer *c = dynamic_cast<CalibrationDataHistogramContainer *>(craw);
+
+    Analysis::CalibrationDataVariables v;
+    v.jetPt = 50.e3;
+    v.jetEta = -1.1;
+    v.jetAuthor = "AntiKt4Topo";
+
+    TObject *obj = nullptr;
+    double r;
+    CalibrationDataContainer::CalibrationStatus stat = c->getResult(v, r, obj);
+
+    CPPUNIT_ASSERT_EQUAL (CalibrationDataContainer::kSuccess, stat);
+    CPPUNIT_ASSERT_DOUBLES_EQUAL (1.1, r, 0.001);
+
+    stat = c->getStatUncertainty(v, r);
+    CPPUNIT_ASSERT_EQUAL (CalibrationDataContainer::kSuccess, stat);
+    CPPUNIT_ASSERT_DOUBLES_EQUAL (0.2, r, 0.001);
+
+    map<string, UncertaintyResult> allerrors;
+    stat = c->getUncertainties(v, allerrors);
+    for (map<string, UncertaintyResult>::const_iterator itr = allerrors.begin(); itr != allerrors.end(); itr++) {
+      cout << " --> Error " << itr->first << endl;
+    }
+    CPPUNIT_ASSERT_EQUAL((size_t)1+3, allerrors.size());
+
+    CPPUNIT_ASSERT(allerrors.find("cerr") != allerrors.end());
+    CPPUNIT_ASSERT(allerrors.find("result") != allerrors.end());
+    CPPUNIT_ASSERT(allerrors.find("statistics") != allerrors.end());
+    CPPUNIT_ASSERT(allerrors.find("systematics") != allerrors.end());
+  }
+
+  // Make sure we fail correctly if we ask for a number outside the
+  // range that the CDI was made with.
+  void testBeyondTheEdge()
+  {
+    CPPUNIT_ASSERT(false);
   }
 };
 
